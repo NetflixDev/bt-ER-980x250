888041f36075dfc452a50e4a6bbfd3a61bca4302
Greg Connell <greg.connell@ff0000.com>
2019-05-22 17:02:22 -0700

    remove builder.label

--- dist/1-build/980x250/_c20-tracker/883f6267-8d23-4d31-92d9-e8efdd7023de.js
+++ dist/1-build/980x250/_c20-tracker/883f6267-8d23-4d31-92d9-e8efdd7023de.js
@@ -10,7 +10,6 @@ module.exports = {
   "description": "C20/Monet-compatible templates that use  https://github.com/ff0000-ad-tech/ad-docs/blob/master/README.md",
   "type": "build_templates",
   "builder": {
-    "label": "980x250 NF Global Template",
     "tier": "endframe-resolve",
     "size": "980x250",
     "creative": "./dist/3-traffic/default/980x250",
@@ -26,7 +25,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1558481538336,
+  "updated": 1558569726946,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-980x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-980x250",
