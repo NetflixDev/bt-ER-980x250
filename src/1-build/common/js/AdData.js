e824695208a370e96bd07abf6cb639909a2dc65a
Derek Miranda <derek.miranda@ff0000.com>
2019-10-16 11:24:31 -0700

    Using upper-snake case field names

--- src/1-build/common/js/AdData.js
+++ src/1-build/common/js/AdData.js
@@ -1,15 +1,16 @@
-import { ImageManager } from "ad-control";
-import { MonetUtils } from "ad-utils";
+import { ImageManager } from 'ad-control'
+import { MonetUtils } from 'ad-utils'
+
 
 export default function AdData() {
-  var self = this;
+	var self = this
 
-  /**
+	/**
 		EXTRACT JSON DATA
 		Prepare dynamic data here.
 	 */
 
-  /**
+	/**
 		DYNAMIC IMAGES
 		Dynamically loaded images need to be in their own directory, like "dynamic_images/".
 
@@ -19,37 +20,40 @@ export default function AdData() {
 		self.theImageName = ImageManager.addToLoad(adParams.imagesPath + 'sample.jpg');
 	 */
 
-  self.fonts = {
-    primary: "template_font"
-  };
 
-  self.colors = {
-    white: "#F5F5F1",
-    grey: "#221F1F",
-    red: "#E50914",
-    black: "#000000"
-  };
+	self.fonts = {
+		primary: 'template_font'
 
-  // Store svg markup for use in all UISvg instances, reduces duplicate code across builds.  See UISvg.
-  self.svg = {};
 
-  // indicates whether previewing on C2.0 Builder
-  const previewParam = window.getQueryParams().builder_preview;
-  self.inBuilder = !!previewParam && previewParam !== "false";
+	}
 
-  // treatment data
-  self.useSupercut =
-    MonetUtils.getDataByKey("Toggle_Supercut") &&
-    MonetUtils.getDataByKey("Supercut_Video");
-  self.isRTL = window.Monet && window.Monet.isRTL();
-  self.hasTT = !!MonetUtils.getDataByKey("Title_Image");
-  self.hasRatings =
-    self.inBuilder || !!MonetUtils.getDataByKey("Ratings_Bug_Image");
-  self.ratingsKey = self.inBuilder
-    ? "treatment-assets/ratings_bug_fpo.png"
-    : "Ratings_Bug";
-  self.titleText = MonetUtils.getDataByKey("Title_Text");
-  self.headlineText = MonetUtils.getDataByKey("Headline_Text");
-  self.descriptionText = MonetUtils.getDataByKey("Description_Text");
-  self.retinaTT = true;
+	self.colors = {
+		white: '#F5F5F1',
+		grey: '#221F1F',
+		red: '#E50914',
+		black: '#000000',
+
+	}
+
+	// Store svg markup for use in all UISvg instances, reduces duplicate code across builds.  See UISvg.
+	self.svg = {
+
+	}
+
+// indicates whether previewing on C2.0 Builder
+const previewParam = window.getQueryParams().builder_preview
+self.inBuilder = !!previewParam && previewParam !== 'false'
+
+// treatment data
+self.useSupercut = MonetUtils.getDataByKey('TOGGLE_SUPERCUT') && MonetUtils.getDataByKey('SUPERCUT_VIDEO')
+self.isRTL = window.Monet && window.Monet.isRTL()
+self.hasTT = !!MonetUtils.getDataByKey('TITLE_TREATMENT_IMAGE')
+self.hasRatings = self.inBuilder || !!MonetUtils.getDataByKey('RATINGS')
+self.ratingsKey = self.inBuilder ? 'treatment-assets/ratings_bug_fpo.png' : 'RATINGS'
+self.titleText = MonetUtils.getDataByKey('TITLE')
+self.headlineText = MonetUtils.getDataByKey('HEADLINE')
+self.descriptionText = MonetUtils.getDataByKey('DESCRIPTION')
+self.retinaTT = true
+
+
 }
--- src/1-build/common/js/EndFrame/inits/index.js
+++ src/1-build/common/js/EndFrame/inits/index.js
@@ -1,36 +1,28 @@
-import { Styles, Markup, Align, Effects } from "ad-view";
-import { ImageManager } from "ad-control";
-import { Animation } from "@common/js/Animation.js";
-import { Control } from "@common/js/Control.js";
-import CanvasIris from "@common/js/CanvasIris.js";
-import {
-  UIComponent,
-  UIBorder,
-  UIButton,
-  UIImage,
-  TextFormat,
-  UITextField,
-  UISvg
-} from "ad-ui";
-import { ObjectUtils } from "ad-utils";
-import baseInit from "./baseInit.js";
+import { Styles, Markup, Align, Effects } from 'ad-view'
+import { ImageManager } from 'ad-control'
+import { Animation } from '@common/js/Animation.js'
+import { Control } from '@common/js/Control.js'
+import CanvasIris from '@common/js/CanvasIris.js'
+import { UIComponent, UIBorder, UIButton, UIImage, TextFormat, UITextField, UISvg } from 'ad-ui'
+import { ObjectUtils } from 'ad-utils'
+import baseInit from './baseInit.js'
 
-export { mainInit, stackedInit };
+export { mainInit, stackedInit }
 
 function stackedInit(T) {
-  baseInit(T, {
-    logoWidth: 118,
-    ctaWidth: 122,
-    ctaMaxWidth: 122,
-    ctaHeight: 33
-  });
+	baseInit(T, {
+		logoWidth: 118,
+		ctaWidth: 122,
+		ctaMaxWidth: 122,
+		ctaHeight: 33
+	})
 }
 
 function mainInit(T) {
-  baseInit(T, {
-    logoWidth: 112,
-    ctaWidth: 116,
-    ctaMaxWidth: 130,
-    ctaHeight: 30
-  });
+	baseInit(T, {
+		logoWidth: 112,
+		ctaWidth: 116,
+		ctaMaxWidth: 130,
+		ctaHeight: 30
+	})
 }
