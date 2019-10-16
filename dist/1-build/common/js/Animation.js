d0753fc9ba3983e128f38834d3ea99379f5ec5c2
Derek Miranda <derek.miranda@ff0000.com>
2019-10-16 11:24:59 -0700

    C2.0 BT redist

--- dist/1-build/common/js/Animation.js
+++ dist/1-build/common/js/Animation.js
@@ -1,59 +1,62 @@
-import { Styles, Markup, Align, Effects } from "ad-view";
-import { Device } from "ad-external";
+import { Styles, Markup, Align, Effects } from 'ad-view'
+import { Device } from 'ad-external'
+
 
 export class Animation {
-  static start() {
-    console.log("Animation.start()");
-    // show the main container
-    TweenLite.delayedCall(0.1, () => {
-      global.removePreloader();
-    });
-    TweenLite.set(View.main, { opacity: 1 });
+	static start() {
+		console.log('Animation.start()')
+		// show the main container
+TweenLite.delayedCall(0.1, () => {
+	global.removePreloader()
+})
+TweenLite.set(View.main, { opacity: 1 })
 
-    // netflix-ribbon animation at end of zoom always last this long
-    const RIBBON_ANIM_TIME = 0.6;
 
-    const ribbonStart = (window.Creative && Creative.zoomDuration) || 1.7;
-    const initZoomDuration = ribbonStart + RIBBON_ANIM_TIME;
-    const initZoomStart = 0;
-    const initZoomScale = Math.min(
-      (window.Creative && Creative.zoomAmount) || 5,
-      30
-    );
-    const subScale = 1 + (initZoomScale - 1) * 0.03;
-    const timeOffset = initZoomDuration * 0.3;
 
-    if (adData.useSupercut && Device.type === "desktop") {
-      if (View.endFrame.iris) {
-        TweenLite.set(View.endFrame.iris.canvas, { opacity: 0 });
-      }
-      View.endFrame.show();
-      // have Netflix logo already fully in
-      View.endFrame.netflixLogo.progress(1);
+// netflix-ribbon animation at end of zoom always last this long
+const RIBBON_ANIM_TIME = 0.6
 
-      TweenLite.to(View.endFrame, initZoomDuration, {
-        delay: initZoomStart,
-        scale: subScale,
-        ease: Linear.easeNone
-      });
-      TweenLite.to(View.endFrame.subLayer, initZoomDuration - timeOffset, {
-        delay: initZoomStart + timeOffset,
-        scale: initZoomScale - subScale,
-        ease: Expo.easeIn
-      });
+const ribbonStart = (window.Creative && Creative.zoomDuration) || 1.7
+const initZoomDuration = ribbonStart + RIBBON_ANIM_TIME
+const initZoomStart = 0
+const initZoomScale = Math.min((window.Creative && Creative.zoomAmount) || 5, 30)
+const subScale = 1 + (initZoomScale - 1) * 0.03
+const timeOffset = initZoomDuration * 0.3
 
-      TweenLite.delayedCall(ribbonStart, () => {
-        View.ribbon.play();
-      });
-    } else {
-      View.ribbon.play();
-    }
-  }
+if (adData.useSupercut && Device.type === 'desktop') {
+	if (View.endFrame.iris) {
+		TweenLite.set(View.endFrame.iris.canvas, { opacity: 0 })
+	}
+	View.endFrame.show()
+	// have Netflix logo already fully in
+	View.endFrame.netflixLogo.progress(1)
 
-  // IMPORTANT!!! Call this function when your animation is complete!
-  static complete() {
-    console.log("Animation.complete()");
-  }
+	TweenLite.to(View.endFrame, initZoomDuration, {
+		delay: initZoomStart,
+		scale: subScale,
+		ease: Linear.easeNone
+	})
+	TweenLite.to(View.endFrame.subLayer, initZoomDuration - timeOffset, {
+		delay: initZoomStart + timeOffset,
+		scale: initZoomScale - subScale,
+		ease: Expo.easeIn
+	})
+
+	TweenLite.delayedCall(ribbonStart, () => {
+		View.ribbon.play()
+	})
+} else {
+	View.ribbon.play()
+}
+
+
+	}
+
+	// IMPORTANT!!! Call this function when your animation is complete!
+	static complete() {
+		console.log('Animation.complete()')
+
+	}
 
   static playIntro() {
     if (View.intro) {
@@ -80,33 +83,40 @@ export class Animation {
     }
   }
 
-  static showEndFrame() {
-    console.log("Animation.showEndFrame()");
-    if (adData.useSupercut) {
-      // reset endframe after ribbon and supercut
-      View.endFrame.netflixLogo.progress(0);
-      if (View.endFrame.iris) {
-        TweenLite.set(View.endFrame.iris.canvas, { opacity: 1 });
-      }
-      TweenLite.set([View.endFrame, View.endFrame.subLayer], { scale: 1 });
-    }
+	
+	static showEndFrame() {
+    console.log('Animation.showEndFrame()')
+if (adData.useSupercut) {
+	// reset endframe after ribbon and supercut
+	View.endFrame.netflixLogo.progress(0)
+	if (View.endFrame.iris) {
+		TweenLite.set(View.endFrame.iris.canvas, { opacity: 1 })
+	}
+	TweenLite.set([View.endFrame, View.endFrame.subLayer], { scale: 1 })
+}
 
-    if (View.intro) View.intro.hide();
-    View.endFrame.show();
 
-    const creative = new Creative();
-    if (creative.init) {
-      creative.init();
-    }
+        
+		if (View.intro) View.intro.hide()
+		View.endFrame.show()
 
-    if (adData.useSupercut) {
-      creative.play();
-    }
-  }
+const creative = new Creative()
+if (creative.init) {
+	creative.init()
+}
 
-  static playCreative() {
-    console.log("Animation.playCreative()");
-    const creative = new Creative();
-    creative.play();
-  }
+if (adData.useSupercut) {
+	creative.play()
+}
+
+
+	}
+
+	static playCreative() {
+		console.log('Animation.playCreative()')
+		const creative = new Creative()
+		creative.play()
+	}
+
+
 }
--- dist/3-traffic/default/980x250/creative.js
+++ dist/3-traffic/default/980x250/creative.js
@@ -1,34 +1,29 @@
 // how long zoom animates for (in seconds)
-var ZOOM_DURATION = 1.7;
+var ZOOM_DURATION = 1.7
 
 // how much to scale the keyart intro frame
-var ZOOM_AMOUNT = 5;
+var ZOOM_AMOUNT = 5
 
 // how long fade-in takes (in seconds)
-var FADE_DURATION = 1.5;
+var FADE_DURATION = 1.5
 
 var Creative = function() {
-  this.init = function() {
-    console.log("Creative.init()");
-    TweenLite.set(View.endFrame, { alpha: 0 });
-  };
+	this.init = function() {
+		console.log('Creative.init()')
+		TweenLite.set(View.endFrame, { alpha: 0 })
+	}
 
-  this.play = function() {
-    console.log("Creative.play()");
-    var delay = 0;
-    TweenLite.fromTo(
-      View.endFrame,
-      FADE_DURATION,
-      { alpha: 0 },
-      { alpha: 1, delay: delay }
-    );
+	this.play = function() {
+		console.log('Creative.play()')
+		var delay = 0
+		TweenLite.fromTo(View.endFrame, FADE_DURATION, { alpha: 0 }, { alpha: 1, delay: delay })
 
-    TweenLite.delayedCall(delay, function() {
-      View.endFrame.netflixLogo.play();
-    });
-  };
-};
+		TweenLite.delayedCall(delay, function() {
+			View.endFrame.netflixLogo.play()
+		})
+	}
+}
 
 // attaching to Creative class since container looks there for intro zoom properties
-Creative.zoomDuration = ZOOM_DURATION;
-Creative.zoomAmount = ZOOM_AMOUNT;
+Creative.zoomDuration = ZOOM_DURATION
+Creative.zoomAmount = ZOOM_AMOUNT
